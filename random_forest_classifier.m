ca9155e6af8a4993ca04c5b9e5e005cdfb87f548
Alexander Serrano <serrano.alexander94@gmail.com>
2016-05-23 17:45:43 -0400

    Add files via upload
    scores and predicted labels that come out of Random Forest Classifier

--- random_forest_classifier.m
+++ random_forest_classifier.m
@@ -1,4 +1,4 @@
-function [ B ] = random_forest_classifier(N, X, Y)
+function [ B,Yfit,scores,stdevs ] = random_forest_classifier(N, X_train, Y_train,X_test,Y_test)
 %{
 creates an ensemble 'B' of 'N' decision trees for predicting response Y 
 as a function of predictors in the numeric matrix of training data, X. 
@@ -8,8 +8,10 @@ feature.
 MATHWORKS
 ()
 %}
-B = TreeBagger(N,X,Y,'OOBPrediction','On','Method','classification')
+B = TreeBagger(N,X_train,Y_train,'OOBPrediction','On','Method','classification')
 
+
+[Yfit,scores,stdevs] = predict(B,X_test,Y_test);
 
 end
 
