615a234113bc183a2300bfcc2bcdd7adb16a5089
Alexander Serrano <serrano.alexander94@gmail.com>
2016-05-23 17:58:56 -0400

    Update random_forest_classifier.m

--- random_forest_classifier.m
+++ random_forest_classifier.m
@@ -1,4 +1,4 @@
-function [ B,Yfit,scores,stdevs ] = random_forest_classifier(N, X_train, Y_train,X_test,Y_test)
+function [ B,Yfit ] = random_forest_classifier(N, X_train, Y_train,X_test,Y_test)
 %{
 creates an ensemble 'B' of 'N' decision trees for predicting response Y 
 as a function of predictors in the numeric matrix of training data, X. 
@@ -11,7 +11,7 @@ MATHWORKS
 B = TreeBagger(N,X_train,Y_train,'OOBPrediction','On','Method','classification');
 
 
-[Yfit,scores,stdevs] = predict(B,X_test,Y_test);
+Yfit = predict(B,X_test);
 
 end
 
